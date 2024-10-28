43efe9ce9e06ca4746c25f79b5c8efe757c52c5b
Ashok Bhukya <128894041+Alberthoashok@users.noreply.github.com>
2024-10-29 00:32:39 +0530

    Update index.js

--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -1,1 +1,21 @@
+import './index.css'
 
+const UserProfile = props => {
+  const {usersDetails, daleteUser} = props
+
+  const {title, uniqueNo} = usersDetails
+  const onDelete = () => {
+    daleteUser(uniqueNo)
+  }
+
+  return (
+    <li className="container">
+      <p className="title">{title}</p>
+      <button className="button" onClick={onDelete} alt="delete">
+        Delete
+      </button>
+    </li>
+  )
+}
+
+export default UserProfile
